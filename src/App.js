f593da743ff98690a5cc4bbb04e69f769cd31523
Fred Hickey <meanfredblue@yahoo.com>
2023-11-11 15:03:41 -0500

    I learn gooder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          I learn good!
+          I learn gooder and gooder every day!
         </p>
       </header>
     </div>
